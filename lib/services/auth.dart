fa1525a4072abf50e4db3cfc17766e3d25392271
bawcs <dkbac159@gmail.com>
2020-04-20 22:36:33 +0700

    brew crew initial

---
+++ lib/services/auth.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // sign in anonymous
+  Future signInAnonymous() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Sign in with email and password
+
+  // register with email and password
+
+  // Sign out
+}
