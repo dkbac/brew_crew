366e24793291fcb5e1fe8ee2c2011157f73877ef
bawcs <dkbac159@gmail.com>
2020-04-21 21:33:20 +0700

    Implement the toggleView to switch between Sign In and Register as define a void to do that and define a parameter to pass a function through

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
 
 class Register extends StatefulWidget {
+
+  final Function toggleView;
+
+  Register({
+    this.toggleView
+  });
+
   @override
   _RegisterState createState() => _RegisterState();
 }
@@ -21,6 +28,15 @@ class _RegisterState extends State<Register> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         title: Text('Register to Brew Crew'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Sign In'),
+            onPressed: () {
+              widget.toggleView();
+            },
+          ),
+        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
