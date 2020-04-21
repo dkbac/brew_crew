c9d2c60eddefd4a67d035d2aea38612ee8a56f8f
bawcs <dkbac159@gmail.com>
2020-04-21 20:22:04 +0700

    Install a package Provider; Using provider to check the user signed in or signed out; based on the value from stream function we defined in the previous video; Get the user from Provider and maybe will decide to do something after

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:brew_crew/screens/authenticate/sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _user = Provider.of<User>(context);
+
+    print(_user);
     // return either Home or Authenticate widget
     return Authenticate();
   }
