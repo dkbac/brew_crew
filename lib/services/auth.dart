42d89479ef16ce337678e96b9daea6d06d3eac23
bawcs <dkbac159@gmail.com>
2020-04-21 19:58:43 +0700

    Create user class; Define a function return a User object base on the FirebaseUser; Return the User object instead of FirebaseUser

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  // Create user object based on the FirebaseUser
+  User _userFromFirebase(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
   // sign in anonymous
   Future signInAnonymous() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebase(user);
     } catch(e) {
       print(e.toString());
       return null;
