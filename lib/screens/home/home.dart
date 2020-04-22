9a460bd5312ec3c2788142e2813d4b07e6de8dff
bawcs <dkbac159@gmail.com>
2020-04-22 22:24:19 +0700

    Using Stream to get realtime database update; QuerySnapshot

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,26 +1,34 @@
+import 'package:brew_crew/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'brew_list.dart';
 
 class Home extends StatelessWidget {
   final AuthService _auth = new AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        title: Text('Brew Crew'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person), 
-            label: Text('logout'),
-            onPressed: () async {
-             await _auth.signingOut();
-            }, 
-          )
-        ],
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[100],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person), 
+              label: Text('logout'),
+              onPressed: () async {
+               await _auth.signingOut();
+              }, 
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
