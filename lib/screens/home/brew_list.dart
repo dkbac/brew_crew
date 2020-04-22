d271d89852928779830802430797bf345d8f3be6
bawcs <dkbac159@gmail.com>
2020-04-22 22:52:00 +0700

    Create a BrewTile with some design to show the brew list

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
 
 class BrewList extends StatefulWidget {
   @override
@@ -13,8 +14,11 @@ class _BrewListState extends State<BrewList> {
 
     final brews = Provider.of<List<Brew>>(context);
 
-    return Container(
-      
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
     );
   }
 }
